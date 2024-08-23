5354df983b926a75cd25f9a198c2db4e63be3a47
Denis Ilchuk <denis290788@gmail.com>
2024-08-23 23:58:28 +0300

    before review

--- src/services/rootReducer.test.ts
+++ src/services/rootReducer.test.ts
@@ -1,3 +1,4 @@
+import { combineReducers } from '@reduxjs/toolkit';
 import { burgerConstructorSlice } from './slices/burgerConstructor';
 import { feedSlice } from './slices/feed';
 import { ingredientsSlice } from './slices/ingredients';
@@ -6,7 +7,14 @@ import { userSlice } from './slices/user';
 import store from './store';
 
 describe('rootReducer', () => {
-  test('корректно инициализируется rootReducer', () => {
+  const rootReducer = combineReducers({
+    [ingredientsSlice.name]: ingredientsSlice.reducer,
+    [userSlice.name]: userSlice.reducer,
+    [feedSlice.name]: feedSlice.reducer,
+    [burgerConstructorSlice.name]: burgerConstructorSlice.reducer,
+    [orderSlice.name]: orderSlice.reducer
+  });
+  test('корректно инициализируется rootReducer при передаче в стейт undefined и неизвестного экшена', () => {
     const initialState = {
       [ingredientsSlice.name]: ingredientsSlice.getInitialState(),
       [userSlice.name]: userSlice.getInitialState(),
@@ -15,7 +23,7 @@ describe('rootReducer', () => {
       [orderSlice.name]: orderSlice.getInitialState()
     };
 
-    const state = store.getState();
+    const state = rootReducer(undefined, { type: '' });
 
     expect(state).toEqual(initialState);
   });
